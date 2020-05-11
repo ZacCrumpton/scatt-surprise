437ddb71677cb9c35a84e43f1835070c3eeb4873
Zac Crumpton <z.crumpton@gmail.com>
2020-05-11 01:42:08 -0500

    finsihed setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import './App/App.scss';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>SCATT SURPRISE</h2>
+      <button className="btn btn-dark"><i className="fas fa-rocket"></i></button>
     </div>
   );
 }
