39a470a6ef3a94d63bf03912c29183812f2946d1
Zac Crumpton <z.crumpton@gmail.com>
2020-06-01 21:29:17 -0500

    edit route works

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,10 +3,18 @@ import React from 'react';
 import './Home.scss';
 
 class Home extends React.Component {
+  editEvent = (e) => {
+    e.preventDefault();
+    const scattId = '12345';
+    // /edit/12345; <- url that we want.
+    this.props.history.push(`/edit/${scattId}`);
+  }
+
   render() {
     return (
       <div className="Home">
-        <h1>THis is Home</h1>
+        <h1>Home</h1>
+        <button className="btn btn-dark" onClick={this.editEvent}>Edit a Thing</button>
       </div>
     );
   }
