ae19a1c484dcfcc2fcc2db8a7ecfed8012951979
Zac Crumpton <z.crumpton@gmail.com>
2020-06-10 19:48:36 -0500

    can see empty array

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import ScattCard from '../../shared/ScattCard/ScattCard';
 
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    scatts: [],
+  }
+
   editEvent = (e) => {
     e.preventDefault();
     const scattId = '12345';
@@ -13,12 +17,17 @@ class Home extends React.Component {
   }
 
   render() {
+    const { scatts } = this.state;
+    const buildScattCards = scatts.map((scatt) => (
+      <ScattCard scatt={scatt} />
+    ));
     return (
       <div className="Home">
         <h1>Home</h1>
         <button className="btn btn-dark" onClick={this.editEvent}>Edit a Thing</button>
-        <Link to='/scatts/54325432' >View Single</Link>
-        <Link to='/new' >View NEW</Link>
+          <div className="d-flex flex-wrap">
+            {buildScattCards}
+          </div>
       </div>
     );
   }
