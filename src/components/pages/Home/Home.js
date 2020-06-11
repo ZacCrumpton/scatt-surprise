d181b3bfab54537586c6f1f076a37ade4bfa5bc1
Zac Crumpton <z.crumpton@gmail.com>
2020-06-10 23:02:12 -0500

    scatt cards print

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import authData from '../../../helpers/data/authData';
+import scattData from '../../../helpers/data/scattData';
 import ScattCard from '../../shared/ScattCard/ScattCard';
 
 import './Home.scss';
@@ -9,22 +11,25 @@ class Home extends React.Component {
     scatts: [],
   }
 
-  editEvent = (e) => {
-    e.preventDefault();
-    const scattId = '12345';
-    // /edit/12345; <- url that we want.
-    this.props.history.push(`/edit/${scattId}`);
+  getScatts = () => {
+    const uid = authData.getUid();
+    scattData.getScattsByUid(uid)
+      .then((scatts) => this.setState({ scatts }))
+      .catch((err) => console.error('unable to get scats: ', err));
+  }
+
+  componentDidMount() {
+    this.getScatts();
   }
 
   render() {
     const { scatts } = this.state;
     const buildScattCards = scatts.map((scatt) => (
-      <ScattCard scatt={scatt} />
+      <ScattCard key={scatt.id} scatt={scatt} />
     ));
     return (
       <div className="Home">
         <h1>Home</h1>
-        <button className="btn btn-dark" onClick={this.editEvent}>Edit a Thing</button>
           <div className="d-flex flex-wrap">
             {buildScattCards}
           </div>
